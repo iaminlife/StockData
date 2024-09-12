2e4593f7e71d208174a5c2b50ec398820baf0f50
iaminlife <151754552+iaminlife@users.noreply.github.com>
2024-09-12 19:52:47 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const apiKey = 'NTB2LIKDMQ9N9SEX'; // Your API Key
+const apiKey = 'SA4NZ0EX5IDIEQO0'; // Your API Key
 const stockSymbols = ['SOXX', 'XAUUSD', 'VNQI', 'ABBV', 'CAMT', 'MSFT'];
 const chartIds = ['soxxChart', 'xauChart', 'vnqiChart', 'abbvChart', 'camtChart', 'msftChart'];
 const colors = ['rgba(75, 192, 192, 1)', 'rgba(255, 215, 0, 1)', 'rgba(255, 99, 132, 1)', 'rgba(255, 159, 64, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 205, 86, 1)'];
